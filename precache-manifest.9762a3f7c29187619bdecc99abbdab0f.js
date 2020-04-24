3ae53139365789d8db29ca509ef6169f8dcff7c2
Dian Yuan <yuan_dian@live.com>
2020-04-24 15:25:30 -0700

    Updates

---
+++ precache-manifest.9762a3f7c29187619bdecc99abbdab0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081fc33097e80f6e5528a5b80296cc49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d993234cccc2002bd55a",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "2eb2853e3a20a37ce702",
+    "url": "/static/css/main.ca412afa.chunk.css"
+  },
+  {
+    "revision": "d993234cccc2002bd55a",
+    "url": "/static/js/2.a8632d16.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/static/js/2.a8632d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb2853e3a20a37ce702",
+    "url": "/static/js/main.b5441a6d.chunk.js"
+  },
+  {
+    "revision": "1f78a66bd0a7911a5180",
+    "url": "/static/js/runtime-main.ccad202e.js"
+  }
+]);
